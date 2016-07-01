630915fb178a911cdba6d68f45aa8b72bff0996f
David Elliott <david@philosophie.is>
2016-06-30 18:50:44 -0700

    customize some stuff

--- config/spring.rb
+++ config/spring.rb
@@ -1,3 +1,22 @@
+require 'spring/application'
+
+class Spring::Application
+  alias connect_database_orig connect_database
+
+  def connect_database
+    disconnect_database
+    reconfigure_database
+    connect_database_orig
+  end
+
+  def reconfigure_database
+    if active_record_configured?
+      ActiveRecord::Base.configurations =
+        Rails.application.config.database_configuration
+    end
+  end
+end
+
 %w(
   .ruby-version
   .rbenv-vars
